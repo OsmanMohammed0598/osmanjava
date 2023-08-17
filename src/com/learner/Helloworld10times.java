b1280e3a665fefba24f3e956979c062a62f23d7c
Osmanmohammed0598 <osmanmohammed0598@gmail.com>
2023-08-17 09:18:03 -0500

    Helloworld10times

---
+++ src/com/learner/Helloworld10times.java
@@ -0,0 +1,11 @@
+package com.learner;
+
+public class Helloworld10times {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
+
+
